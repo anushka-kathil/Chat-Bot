f7040eea5971d38b4d174ad29c7d3db12c12686a
anushka-kathil <149619819+anushka-kathil@users.noreply.github.com>
2025-01-11 12:29:40 +0530

    Finished

--- app/src/main/java/com/example/chatbot/ChatBotUiState.kt
+++ app/src/main/java/com/example/chatbot/ChatBotUiState.kt
@@ -4,7 +4,7 @@ sealed interface ChatBotUiState {
     data object Ideal:ChatBotUiState
     data object Loading:ChatBotUiState
 
-    data class Success(val ChatData:String) : ChatBotUiState
-    data class Error(val ChatError:String) : ChatBotUiState
+    data class Success(val chatData:String) : ChatBotUiState
+    data class Error(val chatError:String) : ChatBotUiState
 
 }
